7d7ede00675bec3a3d197b69b60095ea97a4991b
Frank Wanye <wanyef@mail.gvsu.edu>
2018-06-24 13:02:40 +0000

    Added Makefile (also got impromptu compile lesson I'll prolly forget)

--- openmp/value_of_pi.c
+++ openmp/value_of_pi.c
@@ -1,3 +1,12 @@
 /*
  * Estimating the value of Pi using the circle-in-square method.
  */
+# include <stdlib.h>
+# include <stdio.h>
+# include <limits.h>
+
+int main(int argc, char* argv[]) {
+    srandom(1995);  // Seed the random number generator
+    int n_attempts = INT_MAX;
+    printf("Number of attemps = %d\n", n_attempts);
+}
