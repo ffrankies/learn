83272ecf159a5d71f3700d1b8312c2b67825e95e
Frank Wanye <wanyef@mail.gvsu.edu>
2018-06-25 10:14:04 +0000

    Added sequential implementation

--- openmp/array_proc.c
+++ openmp/array_proc.c
@@ -21,9 +21,26 @@ void init_2d_array(double array[][ARR_SIZE]) {
     }
 }
 
+/**
+ * Performs a sequential sum across the 2d array.
+ */
+void sequential_impl(double data[][ARR_SIZE]) {
+    printf("======Sequential implementation======\n");
+    init_2d_array(data);
+    clock_t start = clock();
+    long double sum = 0;
+    for (int row = 0; row < ARR_SIZE; ++row) {
+        for (int col = 0; col < ARR_SIZE; ++col) {
+            sum += data[row][col];
+        }
+    }
+    printf("Took %ld clock ticks to run.\n", clock() - start);
+    printf("Sum = %Lf\n", sum);
+}
+
 int main(int argc, char *argv[]) {
     printf("hello!\n");
     static double data[ARR_SIZE][ARR_SIZE];  // Without static, get seg fault
-    init_2d_array(data);
+    sequential_impl(data);
     return 0;
 }
