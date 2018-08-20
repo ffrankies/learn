a8dce1a0ceb15ffe44bc9a0669de972a6316e3ec
Frank Wanye <wanyef@mail.gvsu.edu>
2018-08-20 12:46:46 -0400

    Getting started with MPI

---
+++ mpi/hello_world/hello_world.c
@@ -0,0 +1,21 @@
+#include "hello_world.h"
+
+int main(int argc, char* argv[]) {
+
+    int numtasks, taskid, hostnamelen;
+    char host[MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+    MPI_Get_processor_name(taskid, &hostnamelen);
+    
+    print("Hello World! I'm task %d, running on host %s", taskid, host);
+
+    if (taskid == MASTER_TASK) {
+        print("I'm the master task. There are %d tasks running.", numtasks);
+    }
+
+    MPI_Finalize();
+
+}
---
+++ mpi/hello_world/hello_world.h
@@ -0,0 +1,10 @@
+#ifndef H_WORLD_HELLO
+#define H_WORLD_HELLO
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MASTER_TASK 0
+
+#endif // H_WORLD_HELLO
