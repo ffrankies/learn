08256ae92b1683b74a8584325ded40b3fcd0b414
Frank Wanye <wanyef@mail.gvsu.edu>
2018-08-20 12:53:40 -0400

    Bugfix

--- mpi/hello_world/hello_world.c
+++ mpi/hello_world/hello_world.c
@@ -8,12 +8,12 @@ int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
-    MPI_Get_processor_name(taskid, &hostnamelen);
+    MPI_Get_processor_name(host, &hostnamelen);
     
-    print("Hello World! I'm task %d, running on host %s", taskid, host);
+    printf("Hello World! I'm task %d, running on host %s", taskid, host);
 
     if (taskid == MASTER_TASK) {
-        print("I'm the master task. There are %d tasks running.", numtasks);
+        printf("I'm the master task. There are %d tasks running.", numtasks);
     }
 
     MPI_Finalize();
